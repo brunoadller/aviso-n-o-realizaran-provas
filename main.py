c4bcc55763fa815e22c876e5518ad7b532095f44
brunoadller <adller676@gmail.com>
2025-11-13 18:28:57 -0300

    create loop for message

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import pandas as pd
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from  process_phone_numbers_sheet import process_phone_numbers_sheet
 
 
 relatorio_nao_fizeram_provas = process_phone_numbers_sheet()
 
-print(relatorio_nao_fizeram_provas)
+contatos_df = pd.DataFrame(relatorio_nao_fizeram_provas)
+contatos_df.to_excel('nao_realizaram_provas.xlsx', index=False)
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager))
+driver.get('https://web.whatsapp.com')
+
+
+
+dados = pd.read_excel('nao_realizaram_provas.xlsx')
+
+for index, row in dados.iterrows():
+    numero = str(row['Telefone'])
